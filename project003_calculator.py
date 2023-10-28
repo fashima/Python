384b2f265cfff21612983ac1dedd90aa1e451c15
fashima <fabricioshimabuku@gmail.com>
2023-10-28 02:04:33 -0300

    Updated Project 003 and README.md

--- project003_calculator.py
+++ project003_calculator.py
@@ -1,37 +1,121 @@
+import os
+
+def main_menu():
+    print("0 : Addition")
+    print("1 : Subtraction")
+    print("2 : multiplication")
+    print("3 : Division")
+    print("4 : Exponentiation")
+    print("\n")
+    print("Which of the mathematical operations would you like to calculate?")   
+
 while True:
-    print("**********************")
-    print("CALCULATOR")
-    print("**********************")
-
-    first_number = float(input("Type the first number: "))
-    secound_number = float(input("Type the secound number: "))
-
-    print("CHOOSE THE OPERATION")
-    print("1 - ADITION")
-    print("2 - SUBTRACTION")
-    print("3 - MULTIPLICATION")
-    print("4 - DIVISION")
-    print("0 - EXIT")
-
-    menu_select = int(input("Type the number: "))
-
-    if menu_select == 1:
-        result = first_number + secound_number
-        print("Result: ", result)
-    elif menu_select == 2:
-        result = first_number - secound_number
-        print("Result: ", result)
-    elif menu_select == 3:
-        result = first_number * secound_number
-        print("Result: ", result)
-    elif menu_select == 4:
-        if secound_number != 0:
-            result = first_number / secound_number
-            print("Result: ", result)
+    main_menu()
+    menu = int(input())
+    if menu == 0:
+        print(">>> + was chosen")
+        print("\n")
+        print("Input the first value:")
+        n1 = int(input())
+        print("\n")
+        print("input the secound value:")
+        n2 = int(input())
+        print("\n")
+        addition = n1 + n2
+        print(f"{n1} + {n2} = {addition} ")
+        print(" ")
+        print("=" * 20)
+        print("Would you like to perform another mathematical operation? 0 - YES, 1 - NO")
+        option = int(input())
+        if option == 0:
+            os.system('cls')
+        elif option == 1:
+            break
         else:
-            print("Error: Divison by 0")
-    elif menu_select == 0:
-        print("Logout...")
-        break
+            print("Invalid Option")
+    elif menu == 1:
+        print(">>> - was chosen")
+        print("\n")
+        print("Input the first value:")
+        n1 = int(input())
+        print("\n")
+        print("Input the secound value:")
+        n2 = int(input())
+        print("\n")
+        subtraction = n1 - n2
+        print(f"{n1} - {n2} = {subtraction} ")
+        print(" ")
+        print("=" * 20)
+        print("Would you like to perform another mathematical operation? 0 - YES, 1 - NO")
+        opcao = int(input())
+        if option == 0:
+            os.system('cls')
+        elif option == 1:
+            break
+        else:
+            print("Invalid Option")
+    elif menu == 2:
+        print(">>> * was chosen")
+        print("\n")
+        print("Input the first value:")
+        n1 = int(input())
+        print("\n")
+        print("Input the secound value:")
+        n2 = int(input())
+        print("\n")
+        multiplication = n1 * n2
+        print(f"{n1} * {n2} = {multiplication} ")
+        print(" ")
+        print("=" * 20)
+        print("Would you like to perform another mathematical operation? 0 - YES, 1 - NO")
+        option = int(input())
+        if option == 0:
+            os.system('cls')
+        elif option == 1:
+            break
+        else:
+            print("Invalid Option")
+    elif menu == 3:
+        print(">>> / was chosen")
+        print("\n")
+        print("Input the first value:")
+        n1 = int(input())
+        print("\n")
+        print("Input the secound value:")
+        n2 = int(input())
+        print("\n")
+        division = n1 / n2
+        print(f"{n1} / {n2} = {division} ")
+        print(" ")
+        print("=" * 20)
+        print("Would you like to perform another mathematical operation? 0 - YES, 1 - NO")
+        option = int(input())
+        if option == 0:
+            os.system('cls')
+        elif option == 1:
+            break
+        else:
+            print("Invalid Option")
+    elif menu == 4:
+        print(">>> ** was chosen")
+        print("\n")
+        print("Input the first value:")
+        n1 = int(input())
+        print("\n")
+        print("Input the secound value:")
+        n2 = int(input())
+        print("\n")
+        exponentiation = n1 ** n2
+        print(f"{n1} ** {n2} = {exponentiation} ")
+        print(" ")
+        print("=" * 20)
+        print("Would you like to perform another mathematical operation? 0 - YES, 1 - NO")
+        option = int(input())
+        if option == 0:
+            os.system('cls')
+        elif option == 1:
+            break
+        else:
+            print("Invalid Option")
     else:
-        print("It's not a valid option, please try again")
+        print("Invalid Option")
